e2ca2ef326663df9c666e4436b138dd1b7ce9d06
yui masuko <yui.t.tom@gmail.com>
2021-08-05 15:00:02 +0900

    見た目文字修正

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -1,40 +1,34 @@
 class EstatesController < ApplicationController
   before_action :set_estate, only: %i[ show edit update destroy ]
 
-  # GET /estates or /estates.json
   def index
     @estates = Estate.all
   end
 
-  # GET /estates/1 or /estates/1.json
   def show
+    @estate = Estate.find(params[:id])
+    @stations = @estate.stations
   end
 
-  # GET /estates/new
   def new
     @estate = Estate.new
   end
 
-  # GET /estates/1/edit
   def edit
+    @estate = Estate.find(params[:id])
   end
 
   # POST /estates or /estates.json
   def create
     @estate = Estate.new(estate_params)
-
-    respond_to do |format|
-      if @estate.save
-        format.html { redirect_to @estate, notice: "Estate was successfully created." }
-        format.json { render :show, status: :created, location: @estate }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @estate.errors, status: :unprocessable_entity }
-      end
+    if @estate.save
+      flash[:notice] = "登録が完了しました！"
+      redirect_to estate_path(@estate.id)
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /estates/1 or /estates/1.json
   def update
     respond_to do |format|
       if @estate.update(estate_params)
@@ -47,23 +41,20 @@ class EstatesController < ApplicationController
     end
   end
 
-  # DELETE /estates/1 or /estates/1.json
   def destroy
     @estate.destroy
     respond_to do |format|
-      format.html { redirect_to estates_url, notice: "Estate was successfully destroyed." }
+      format.html { redirect_to estates_url, notice: "情報を削除しました！" }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_estate
-      @estate = Estate.find(params[:id])
-    end
+  def set_estate
+    @estate = Estate.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def estate_params
-      params.require(:estate).permit(:name, :price, :adress, :year, :content)
-    end
+  def estate_params
+    params.require(:estate).permit(:name, :price, :adress, :year, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'estates#index'
   resources :estates
+    get :stations, on: :collection
 end
